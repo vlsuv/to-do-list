7c0f780f8227c5988cf3744f2996c96846a24cde
Vlad Suchkov <vsuchkov96@gmail.com>
2021-07-13 10:26:01 +0400

    initial commit

---
+++ to-do-list/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  to-do-list
+//
+//  Created by vlsuv on 13.07.2021.
+//  Copyright © 2021 vlsuv. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
